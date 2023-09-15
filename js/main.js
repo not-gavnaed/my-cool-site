011730d0dd177ed88109cb1da08a6c474a1f1a53
not-gavnaed <114421346+not-gavnaed@users.noreply.github.com>
2023-09-15 20:03:00 +0300

    Добавил отсчет времени

--- js/main.js
+++ js/main.js
@@ -68,6 +68,33 @@ YT.ready((event=> {
     })
 }))
 
+
+function countdown() {
+    const targetDate = new Date("2024-01-27 00:00:00").getTime(); 
+
+    const interval = setInterval(function() {
+        const currentDate = new Date().getTime();
+        const timeLeft = targetDate - currentDate;
+
+        if (timeLeft <= 0) {
+            clearInterval(interval);
+            document.getElementById("countdown").innerHTML = "Время истекло!";
+        } else {
+            const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+            document.getElementById("days").innerHTML = days;
+            document.getElementById("hours").innerHTML = hours;
+            document.getElementById("minutes").innerHTML = minutes;
+            document.getElementById("seconds").innerHTML = seconds;
+        }
+    }, 1000);
+}
+
+countdown();
+
 // const nick = ['𝙦𝙪𝙞𝙯','𝙦𝙪𝙞','𝙦𝙪','𝙦','𝙦𝙪','𝙦𝙪𝙞']
 // let i = 0
 // setInterval(()=> {
